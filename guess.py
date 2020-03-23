cf31fac98bfbadd1bba68a7b186cdad82fa2b829
Ou Frank <oufrank@Oude-MacBook-Pro.local>
2020-03-23 17:49:02 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,8 +1,9 @@
 import random
 
 r=random.randint(1,100)
-
+count = 0
 while True:
+	count += 1
 	num = input('請猜數字:')
 	num = int(num)
 	if num == r:
@@ -12,3 +13,4 @@ while True:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('這是你猜第',count,'次')
